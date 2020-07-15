e7966d85469545ead83da6289421d6efd4c2d767
trainto <trainto@gmail.com>
2020-07-15 14:46:51 +0900

    Add home as page

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = React.memo(() => {
+  return (
+    <div>
+      <ul>
+        <li>시작</li>
+        <li>점수판</li>
+      </ul>
+    </div>
+  );
+});
+
+export default Home;
