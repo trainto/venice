6ce61b7fae6345ce2a1faec69b441ba2875cc743
trainto <trainto@gmail.com>
2020-07-16 18:22:22 +0900

    Add menu component

---
+++ src/components/menu/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div>
+      <button type="button" className="button">
+        시작
+      </button>
+    </div>
+  );
+};
+
+export default Menu;
