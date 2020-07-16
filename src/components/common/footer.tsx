fe51df80df1da3c2bf2b64bf452daaea1bfb8df5
trainto <trainto@gmail.com>
2020-07-16 16:49:16 +0900

    Footer: Add icon and copyright

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Store from '../../store';
 
 const Footer = React.memo(() => {
+  const store = Store.useStore();
+
   return (
     <footer className="px-5">
       <hr />
-      Footer
+      <a href="https://github.com/trainto/venice" target="_blank" rel="noopener noreferrer">
+        <img src={require(`../../assets/github-${store.theme}.png`)} alt="github" width={24} height={24} />
+      </a>
+
+      <span className="float-right text-muted">trainto ©2020</span>
     </footer>
   );
 });
