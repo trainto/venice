e7bc8eb0305179acac3ce4744d9993976fa724bf
trainto <trainto@gmail.com>
2020-07-17 13:54:06 +0900

    Input always has to have focus on it

--- src/components/game/input.tsx
+++ src/components/game/input.tsx
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const Input = React.memo(({ onEnter }: { onEnter: (input: string) => void }) => {
   const [value, setValue] = useState('');
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
 
   const handleEnter = (e: React.KeyboardEvent) => {
     if (e.key === 'Enter') {
@@ -18,6 +26,12 @@ const Input = React.memo(({ onEnter }: { onEnter: (input: string) => void }) => 
         value={value}
         onChange={(e) => setValue(e.target.value)}
         onKeyPress={handleEnter}
+        ref={inputRef}
+        onBlur={() => {
+          if (inputRef.current) {
+            inputRef.current.focus();
+          }
+        }}
       />
       <div className="input-group-append">
         <button
