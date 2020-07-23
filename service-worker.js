061716438329e7bf558e6f618a43417ac6b4259c
trainto <trainto@gmail.com>
2020-07-23 17:14:51 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7ec7cb827439d6fe6d2bd989ae70e56.js"
+  "/precache-manifest.193a9926419bc57cab9695291fe67dff.js"
 );
 
 self.addEventListener('message', (event) => {
